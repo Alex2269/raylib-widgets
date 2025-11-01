bacc16ee5f0c6a3985a00dd7818cc11405935eac
Aleksander <servaL24@i.ua>
2025-11-01 23:22:28 +0200

    2025-11-01

--- raylib-widgets-3/raylib-Gui_Spinner-raster-font/main/main.c
+++ raylib-widgets-3/raylib-Gui_Spinner-raster-font/main/main.c
@@ -9,8 +9,10 @@
 int spacing = 2; // Відстань між символами
 
 // Приклади двох значень: ціле і з рухомою комою
-int intValue = 10;
-float floatValue = 25.5f;
+int iVal1 = 10;
+int iVal2 = 10;
+float fVal1 = 25.5f;
+float fVal2 = 25.5f;
 
 // Мінімальні і максимальні значення для int і float спінерів
 int intMin = 0, intMax = 100;
@@ -26,19 +28,32 @@ int main(void) {
         ClearBackground(RAYWHITE);
 
         // Спінер для int (крок 1)
-        bool changedInt = Gui_Spinner(0, 300, 100, 320, 40, "Менше", "Більше",
-                                      &intValue, &intMin, &intMax,
-                                      1, GUI_SPINNER_INT, BLUE, TerminusBold24x12_font, spacing);
+        Gui_Spinner(0, 300, 100, 320, 40, "Менше", "Більше",
+                    &iVal1, &intMin, &intMax,
+                    1, GUI_SPINNER_INT, GUI_SPINNER_HORIZONTAL,
+                    BLUE, TerminusBold24x12_font, spacing);
 
         // Спінер для float (крок 0.25)
-        bool changedFloat = Gui_Spinner(1, 300, 200, 320, 40, "Менше", "Більше",
-                                        &floatValue, &floatMin, &floatMax,
-                                        0.25f, GUI_SPINNER_FLOAT, GREEN, TerminusBold24x12_font, spacing);
+        Gui_Spinner(1, 300, 200, 320, 40, "Менше", "Більше",
+                    &fVal1, &floatMin, &floatMax,
+                    0.25f, GUI_SPINNER_FLOAT, GUI_SPINNER_HORIZONTAL,
+                    GREEN, TerminusBold24x12_font, spacing);
+
+        Gui_Spinner(2, 100, 175, 20, 240, "Менше", "Більше",
+                    &iVal2, &intMin, &intMax,
+                    1, GUI_SPINNER_INT, GUI_SPINNER_VERTICAL,
+                    BLUE, TerminusBold24x12_font, spacing);
+
+        Gui_Spinner(3, 500, 175, 20, 240, "Менше", "Більше",
+                    &fVal2, &floatMin, &floatMax,
+                    0.5f, GUI_SPINNER_FLOAT, GUI_SPINNER_VERTICAL,
+                    BLUE, TerminusBold24x12_font, spacing);
 
         // Відображення інформації вгорі
         char text[64];
-        snprintf(text, sizeof(text), "intValue: %d  floatValue: %.2f", intValue, floatValue);
-        DrawTextScaled(TerminusBold24x12_font, 20, 20, text, spacing, 1, BLACK);
+        snprintf(text, sizeof(text), "iVal1: %d\niVal2: %d\nfVal1: %.2f\nfVal2: %.2f", iVal1, iVal2, fVal1, fVal2);
+
+        DrawTextScaled(TerminusBold24x12_font, 200, 20, text, spacing, 1, BLACK);
 
         EndDrawing();
     }
